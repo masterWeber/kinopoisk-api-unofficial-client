0a7a6d7ce452d63403fa5a2cf232eae2893d5678
Master Weber <master.weber@outlook.com>
2022-04-13 08:19:23 +0300

    Bumped version number to 2.2.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = [
 
 setuptools.setup(
     name="kinopoisk-api-unofficial-client",
-    version="2.1.1",
+    version="2.2.0",
     author="Master Weber",
     author_email="master.weber@outlook.com",
     description="API Client for the unofficial Kinopoisk api",
